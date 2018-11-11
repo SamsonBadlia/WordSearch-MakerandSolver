a73efee0bc2076db9a380856ab3078762e37c3a1
Samson Badlia <sbadlia00@stuy.edu>
2018-11-11 15:21:05 -0500

    Added more fields and updated toString to have a border

--- WordSearch.java
+++ WordSearch.java
@@ -1,6 +1,22 @@
+import java.util.*; //random, scanner, arraylist
+import java.io.*; //file, filenotfoundexception
+
 public class WordSearch{
 
-    private char[][]data;
+  private char[][]data;
+
+  //the random seed used to produce this WordSearch
+  private int seed;
+
+  //a random Object to unify your random calls
+  private Random randgen;
+
+  //all words from a text file get added to wordsToAdd, indicating that they have not yet been added
+  private ArrayList<String>wordsToAdd;
+
+  //all words that were successfully added get moved into wordsAdded.
+  private ArrayList<String>wordsAdded;
+
 
     /**Initialize the grid to the size specified
      *and fill all of the positions with '_'
@@ -8,6 +24,9 @@ public class WordSearch{
      *@param col is the starting width of the WordSearch
      */
     public WordSearch(int rows,int cols){
+      if (rows < 0 || cols < 0){
+        throw new IllegalArgumentException("Rows/Columns cannot be negative");
+      }
       data = new char[rows][cols];
       for (int i = 0; i < data.length; ++i){
         for (int x = 0; x < data[i].length; ++x){
@@ -25,19 +44,19 @@ public class WordSearch{
       }
     }
 
-    /**Each row is a new line, there is a space between each letter
-     *@return a String with each character separated by spaces, and rows
-     *separated by newlines.
-     */
     public String toString(){
-      String s = "";
-      for (int i = 0; i < data.length; ++i){
-        for (int x = 0; x < data[i].length; ++x){
-          s += data[i][x] + " ";
+      String s = "" ;
+      for (int i = 0 ; i < data.length ; ++i) {
+        s += "|" ;
+        for (int x = 0 ; x < data[i].length ; ++x) {
+          if (x < data[i].length-1) {
+            s += data[i][x] + " ";
+          }
+          else {
+            s += data[i][x];
+          }
         }
-        if (i == data.length - 1){
-          s += "\n";
-        }
+        s+= "|\n";
       }
       return s;
     }
